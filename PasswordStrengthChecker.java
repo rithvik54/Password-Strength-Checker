8c58311d32ebc89ce375945c6310bfebcf8982f1
E Rithvik <rithvikrithvik54@gmail.com>
2025-10-26 20:29:10 +0530

    Implement password strength checker
    This program checks the strength of a password based on length and character variety.

--- PasswordStrengthChecker.java
+++ PasswordStrengthChecker.java
@@ -1,1 +1,36 @@
+import java.util.Scanner;
 
+public class PasswordStrengthChecker {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your password: ");
+        String password = sc.nextLine();
+        sc.close();
+
+        int score = 0;
+
+        if (password.length() >= 8) score++;
+        if (password.matches(".*[A-Z].*")) score++;
+        if (password.matches(".*[a-z].*")) score++;
+        if (password.matches(".*[0-9].*")) score++;
+        if (password.matches(".*[!@#$%^&*(),.?\":{}|<>].*")) score++;
+
+        switch (score) {
+            case 5:
+                System.out.println("Password Strength: 🔥 Very Strong");
+                break;
+            case 4:
+                System.out.println("Password Strength: 💪 Strong");
+                break;
+            case 3:
+                System.out.println("Password Strength: 🙂 Medium");
+                break;
+            case 2:
+                System.out.println("Password Strength: 😐 Weak");
+                break;
+            default:
+                System.out.println("Password Strength: ❌ Very Weak");
+        }
+    }
+}
